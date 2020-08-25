f86ec09d1a3938f1a7f0b398f41d09525b73955e
Sergei Orlov <sorlov+github@gmail.com>
2020-08-24 22:42:17 -0700

    Only click the cancel-autoplay button if it's visible

--- disableYtAutoplay.js
+++ disableYtAutoplay.js
@@ -2,7 +2,9 @@ if (!window.cancelInterval) {
   window.cancelInterval = setInterval(
     () => {
       button = document.getElementsByClassName('ytp-upnext-cancel-button')[0]
-      if (button) { button.click() }
+      // If we find the button, and it is not display:none, click it
+      // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetParent
+      if (button && button.offsetParent) { button.click() }
     },
     4000,
   )
